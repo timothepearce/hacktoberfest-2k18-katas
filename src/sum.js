7d5251bf6b597487210b9e6ef6476d873e2dea02
Timothé Pearce <timothe.pearce@gmail.com>
2018-10-02 16:15:13 +0200

    implement sum, fix #2

--- src/sum.js
+++ src/sum.js
@@ -1,3 +1,13 @@
-export const sum = () => {
-  // TODO
+export const sum = values => {
+  if (!values.length) {
+    return 0;
+  }
+
+  return values.reduce((acc, value) => {
+    if (Number.isInteger(value)) {
+      return acc + value;
+    }
+
+    return acc;
+  });
 };
